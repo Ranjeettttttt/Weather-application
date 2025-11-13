5c2777abb814dcf765e7bcd3b7f83e3f3ce31d10
Ranjeet Bisen <bisenranjeet456@gmail.com>
2025-11-13 13:19:14 +0530

    Weather Forecast Application

---
+++ script.js
@@ -0,0 +1,12 @@
+// Weather Forecast Application
+class WeatherApp {
+    constructor() {
+        this.apiKey = 'efec63400067919846d9e8b8513e3b81'; // Replace with your actual API key
+        this.baseUrl = 'https://api.openweathermap.org/data/2.5';
+        this.isCelsius = true;
+        this.recentCities = JSON.parse(localStorage.getItem('recentCities')) || [];
+        
+        this.initializeElements();
+        this.bindEvents();
+        this.displayRecentCities();
+    }
